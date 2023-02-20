ae3d64ce1d967d70619ea67be4ce43b7b9de5ddc
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-02-20 17:13:11 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    mongoose.set("strictQuery", false);
+    console.log("Connected to mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to 8800");
+});
